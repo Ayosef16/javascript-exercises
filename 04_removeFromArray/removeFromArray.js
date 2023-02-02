f3df36692e31385e4a38d1e63f7d3cd6414f5b00
Ayosef <syrivera@uc.cl>
2023-02-02 18:48:05 -0300

    Do 04-js-exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(a,...theArgs) {
+    for (const arg of theArgs) {
+        if (a.indexOf(arg) < 0) {
+            continue;
+        }
+        else {
+            a.splice(a.indexOf(arg),1);
+        }        
+    };
+    return a;
 };
 
 // Do not edit below this line
