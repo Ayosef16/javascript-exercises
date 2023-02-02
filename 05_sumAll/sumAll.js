4df2db313b60e546088d92b0ab51d0e18165fe71
Ayosef <syrivera@uc.cl>
2023-02-02 19:43:51 -0300

    Do 05-js-exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,21 @@
-const sumAll = function() {
-
-};
+const sumAll = function(a,b) {
+    let sum = 0;
+    if ( a >= 0 && b >=0 && typeof(a) === "number" && typeof(b) === "number") {
+        if ( a <= b) {
+            for (;a <= b; a++) {
+            sum += a;
+            }   
+        }    
+        else {
+            for (;b <= a; b++)
+            sum += b;
+        }
+    return sum;
+    }
+    else {
+        return "ERROR";
+    }
+}
 
 // Do not edit below this line
 module.exports = sumAll;
