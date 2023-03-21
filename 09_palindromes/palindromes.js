0d48f9d5d028f9b79f168a4548a02811b9544c89
Ayosef <syrivera@uc.cl>
2023-03-21 12:08:35 -0300

    Do palindromes exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+    let re = /[\W_]/g;
+    let cleanWord = word.toLowerCase().replace(re,'');
+    reverseWord = cleanWord.split('').reverse().join('');
+    return cleanWord === reverseWord;
 };
 
 // Do not edit below this line
