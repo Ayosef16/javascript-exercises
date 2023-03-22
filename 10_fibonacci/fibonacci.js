fb2fbf01fddffc9600e115130fd65b21a7757234
Ayosef <syrivera@uc.cl>
2023-03-22 10:21:48 -0300

    Do fibonacci exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,16 @@
-const fibonacci = function() {
-
+const fibonacci = function(a) {
+    let fib = [1,1,2,3,5,8];
+    for (i=5 ; i <= 100; i++) {
+        let add = fib[i] + fib[i-1];
+        fib.push(add);
+    }
+    if (a>0) {
+        return fib[a-1];
+    }
+    else {
+        return "OOPS"
+    }
 };
 
-// Do not edit below this line
+// Do not edit below this line 
 module.exports = fibonacci;
