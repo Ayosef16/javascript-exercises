a20bdfbe4c0bc6300b5af23d6fa99668c1a7085d
Ayosef <syrivera@uc.cl>
2023-03-22 11:33:26 -0300

    Do findTheOldest exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,14 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(array) {
+    let oldest = array.sort((a,b) => {
+        if(!a.yearOfDeath) {
+            a.yearOfDeath = new Date().getFullYear();
+        }
+        else if(!b.yearOfDeath) {
+            b.yearOfDeath = new Date().getFullYear();
+        }
+        return a.yearOfDeath - a.yearOfBirth - (b.yearOfDeath - b.yearOfBirth)
+        });
+    return oldest[oldest.length-1];
 };
 
 // Do not edit below this line
